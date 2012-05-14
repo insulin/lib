a91dfc77dcd5c8e94f5e8e64d5771840c639bb8e
Filipe Guerra <alias.mac@gmail.com>
2012-05-14 02:34:51 +0100

    Add license file and header on all files

--- custom/library/Insulin/Relationship/IntegrityException.php
+++ custom/library/Insulin/Relationship/IntegrityException.php
@@ -1,4 +1,15 @@
 <?php
+/*
+ * This file is part of the Insulin Lib.
+ *
+ * (c) Clemente Raposo, Filipe Guerra, João Morais
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license MIT
+ *   See LICENSE.txt shipped with this library.
+ */
 
 require_once 'Insulin/Relationship/Exception.php';
 

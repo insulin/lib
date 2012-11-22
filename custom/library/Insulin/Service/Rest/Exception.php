263768ac62fcd307ade5e4a30fbad2febc2d8c54
Filipe Guerra <alias.mac@gmail.com>
2012-11-22 22:07:38 +0100

    Upgrade Rest Service Client
    Now abstract, and with DRY code.

---
+++ custom/library/Insulin/Service/Rest/Exception.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * This file is part of the Insulin Lib.
+ *
+ * Copyright (c) 2008-2012 Clemente Raposo, Filipe Guerra, João Morais
+ * http://lib.sugarmeetsinsulin.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license MIT
+ *   See LICENSE shipped with this library.
+ */
+
+require_once 'Insulin/Service/Exception.php';
+
+class Insulin_Service_Rest_Exception extends Insulin_Service_Exception
+{
+}
